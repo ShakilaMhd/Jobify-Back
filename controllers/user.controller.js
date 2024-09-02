09520d8f26703a2a3c0614ecc665c31e60da8771
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-09-02 13:08:48 +0330

    complete the user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
             })
         }
         //check role is correct or not
-        if (role === user.role) {
+        if (role !== user.role) {
             return res.status(400).json({
                 message: "Account doesnt exist with current role",
                 success: false
@@ -94,7 +94,7 @@ export const login = async (req, res) => {
 }
 
 //Logout User
-export const logout = async (res, res) => {
+export const logout = async (req, res) => {
     try {
         return res.status(200).cookie("token", "", { maxAge: 0 }).json({
             message: "Logged out successfully.",
@@ -110,15 +110,13 @@ export const updateProfile = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, bio, skills } = req.body;
         const file = req.file
-        if (!fullname || !email || !phoneNumber || !bio || !skills) {
-            return res.status(400).json({
-                message: "Somthing is missing",
-                success: false
-            })
-        }
 
         //cloudinary ayega idhar
-        const skillsArray = skills.split(",")
+        let skillsArray;
+        if (skills) {
+            skillsArray = skills.split(",")
+        }
+        
         const userId = req.id //middleware authentication
         let user = await User.findById(userId)
 
@@ -130,11 +128,11 @@ export const updateProfile = async (req, res) => {
         }
 
         //updating data
-        user.fullname = fullname,
-            user.email = email,
-            user.phoneNumber = phoneNumber,
-            user.profile.bio = bio,
-            user.profile.skills = skillsArray
+        if (fullname) user.fullname = fullname
+        if (email) user.email = email
+        if (phoneNumber) user.phoneNumber = phoneNumber
+        if (bio) user.profile.bio = bio
+        if (skills) user.profile.skills = skillsArray
 
 
         //resume comes here
@@ -150,6 +148,7 @@ export const updateProfile = async (req, res) => {
         }
         return res.status(200).json({
             message: "Profile updated successfully",
+            user,
             success: true
         })
 
--- index.js
+++ index.js
@@ -3,15 +3,13 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import dotenv from "dotenv"
 import connectDB from "./utils/db.js"
+import userRoute from "./routes/user.route.js"
+
 dotenv.config({})
+
 const app = express()
 
-app.get("/home", (req, res) => {
-    return res.status(200).json({
-        message: "I am from backend",
-        success: true
-    })
-})
+
 //middlware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -20,10 +18,30 @@ const corsOptions = {
     origin: 'http//localhost',
     credentials: true
 }
+
 app.use(cors(corsOptions))
+
 const PORT = process.env.PORT || 3000
+
+
+//api
+app.use("/api/v1/user", userRoute)
+
+// "http://localhost:8000/api/v1/user/register"
+// "http://localhost:8000/api/v1/user/login"
+// "http://localhost:8000/api/v1/user/profile/update"
 
 app.listen(PORT, () => {
     connectDB()
     console.log(`server running on port ${PORT}`);
 })
+
+
+
+
+// app.get("/home", (req, res) => {
+//     return res.status(200).json({
+//         message: "I am from backend",
+//         success: true
+//     })
+// })
